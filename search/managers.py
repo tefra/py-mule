3a22b06ee831e9153fe552e5ae2fd56e530de9a7
Chris <chris@komposta.net>
2018-09-09 22:58:52 +0300

    Avoid hitting the resource api for unkown types
    Who knows when I will get to the equipments and translations, for now I want to see how to make the preload process async and thread safe

--- search/managers.py
+++ search/managers.py
@@ -18,25 +18,24 @@ logger = logging.getLogger(__name__)
 
 class ResourceManager:
     ENDPOINT = "http://127.0.0.1:8000/resources"
-    ROUTES = {
-        ResourceCarrier: "airlines",
-        ResourceCabinClass: None,
-        ResourceLocation: "airports",
-        ResourceEquipment: None,
-    }
+    ROUTES = {ResourceCarrier: "airlines", ResourceLocation: "airports"}
 
     def __init__(self, ttl=3600, cachesize=10000):
         self.cache = TTLCache(ttl=ttl, maxsize=cachesize)
 
     def enrich(self, response: SearchResponse):
+        locale = response.locale
         for data in response.data:
             for clazz, resources in data.resources.all():
+                if not self.ROUTES.get(clazz):
+                    continue
+
                 codes = resources.keys()
-                fetch_codes = self.filter_codes(codes)
-                self.fetch(clazz, response.locale, fetch_codes)
+                fetch_codes = self.filter_codes(clazz, locale, codes)
+                self.fetch(clazz, locale, fetch_codes)
 
                 for code in codes:
-                    resources[code] = self.get(clazz, response.locale, code)
+                    resources[code] = self.get(clazz, locale, code)
 
     @cachedmethod(operator.attrgetter("cache"))
     def get(self, clazzz, locale, code):
@@ -46,14 +45,16 @@ class ResourceManager:
         if not codes:
             return []
 
-        if not isinstance(codes, list):
+        if isinstance(codes, str):
             codes = [codes]
 
-        def exists(code):
-            key = hashkey(self, clazzz, locale, code)
-            return key not in self.cache
+        that = self
 
-        return list(filter(exists, codes))
+        def exists(code):
+            key = hashkey(that, clazzz, locale, code)
+            return key not in that.cache
+
+        return list(filter(exists, list(codes)))
 
     def fetch(self, clazzz, locale, code):
         if not code:
